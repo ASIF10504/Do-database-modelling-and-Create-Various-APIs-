9cb6af12c0f13c876cd4e8eb54ede89462b43a4f
ASIF10504 <127234610+ASIF10504@users.noreply.github.com>
2023-04-15 15:51:31 +0530

    Add files via upload

---
+++ database modeling.js
@@ -0,0 +1,62 @@
+{
+    "id": 1296269,
+    "name": "Hello-World",
+    "full_name": "octocat/Hello-World",
+    "owner": {
+        "login": "octocat",
+        "id": 1,
+        "url": "https://api.github.com/users/octocat",
+    },
+    "private": false,
+    "html_url": "https://github.com/octocat/Hello-World",
+    "description": "This your first repo!",
+    "fork": false,
+    "url": "https://api.github.com/repos/octocat/Hello-World",
+    "forks_count": 9,
+    "forks": 9,
+    "stargazers_count": 80,
+    "watchers_count": 80,
+    "watchers": 80,
+    "size": 108,
+    "default_branch": "master",
+    "open_issues_count": 0,
+    "open_issues": 0,
+    "is_template": false,
+    "topics": [
+        "octocat",
+        "atom",
+        "electron",
+        "api"
+    ],
+    "template_repository": {
+        "id": 1296269,
+        "name": "Hello-World-Template",
+        "full_name": "octocat/Hello-World-Template",
+        "owner": {
+            "login": "octocat",
+            "id": 1
+        },
+        "private": false,
+        "html_url": "https://github.com/octocat/Hello-World-Template",
+        "description": "This your first repo!"
+    },
+    "license": {
+        "key": "mit",
+        "name": "MIT License",
+        "spdx_id": "MIT",
+        "url": "https://api.github.com/licenses/mit",
+        "node_id": "MDc6TGljZW5zZW1pdA=="
+    },
+    "organization": {
+        "login": "octocat",
+        "id": 1,
+        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/octocat",
+        "html_url": "https://github.com/octocat",
+    },
+    "license": {},
+    "forks": 1,
+    "open_issues": 1,
+    "watchers": 1
+}
